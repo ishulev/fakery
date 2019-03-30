7285150128f16a26f35c22783e9ee2d3e58abb42
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2019-03-30 23:08:35 +0200

    intro section ready

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -5,14 +5,19 @@ import './Intro.scss';
 class Intro extends Component {
     render() {
         return (
-            <section>
+            <section className="d-flex flex-column section-intro">
+                <div className="flex-fill"></div>
                 <div className="container">
-                    <div className="d-flex flex-row">
-                        <div>
-                        <h2>Подмамени от фалшивото съдържание?</h2>
-                        <p className="mr-md-4">Фалшивите новини са разпространението на информация за несъществуващи факти или слухове, което се прави с цел подвеждане на аудиторията. Предизвикателството, пред което сме изправени днес е, че дезинформация вече е много по-широко разпространено явление. Фалшивите новини са вече добре маскирани с доза истинска информация, което ги прави много по-трудно за разпознаване.</p>
+                    <div className="row">
+                        <div className="col-md-7 col-lg d-flex flex-column">
+                            <h2>Подмамени от фалшивото съдържание?</h2>
+                            <div className="flex-fill purple-padding--top purple-padding--bottom before-background bb--purple">
+                                <p className="mr-md-4">Фалшивите новини са разпространението на информация за несъществуващи факти или слухове, което се прави с цел подвеждане на аудиторията. Предизвикателството, пред което сме изправени днес е, че дезинформация вече е много по-широко разпространено явление. Фалшивите новини са вече добре маскирани с доза истинска информация, което ги прави много по-трудно за разпознаване.</p>
+                            </div>
                         </div>
-                        <img className="float-right" src={plate} alt="Cupcake" />
+                        <div className="d-md-block d-none col-md purple-padding--bottom">
+                            <img className="img-fluid" src={plate} alt="Cupcake" />
+                        </div>
                     </div>
                 </div>
             </section>
